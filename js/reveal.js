d91239de02fa4ef6a006c00181b7abd34c69fee0
Murilo Gotardo <murilogotardo08@gmail.com>
2023-04-25 17:47:50 -0300

    Css ported to Sass

---
+++ js/reveal.js
@@ -0,0 +1,47 @@
+//menu
+
+ScrollReveal().reveal('#sub_1', {
+    delay: 200,
+    duration: 700,
+    reset: true,
+    easing: 'ease-in'
+});
+
+ScrollReveal().reveal('#sub_2', {
+    delay: 500,
+    duration: 700,
+    reset: true,
+    easing: 'ease-in'
+});
+
+ScrollReveal().reveal('#sub_3', {
+    delay: 800,
+    duration: 700,
+    reset: true,
+    easing: 'ease-in'
+});
+
+ScrollReveal().reveal('#sub_4', {
+    delay: 1100,
+    duration: 700,
+    reset: true,
+    easing: 'ease-in'
+});
+
+//notifications
+
+ScrollReveal().reveal('.notification', {
+    delay: 800,
+    duration: 700,
+    reset: true,
+    easing: 'ease-in'
+});
+
+ScrollReveal().reveal('.noti', {
+    delay: 1100,
+    duration: 700,
+    reset: true,
+    easing: 'ease-in'
+});
+
+//schedule
